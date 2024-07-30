8c01204b6b418ab19313f0b67ab1e43c8b39c59f
akanshaa29 <135497931+akanshaa29@users.noreply.github.com>
2024-07-30 17:36:51 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,43 @@
+// // // db.js file
+// // const { Pool } = require("pg");
+// // require("dotenv").config();
+
+// // const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+
+// // const proConfig =  process.env.DATABASE_URL //heroku addon
+
+// // const pool = new Pool({
+// //   connectionString :
+// //    process.env.NODE_ENV === "production" ? proConfig : devConfig
+// // });
+
+// // module.exports = pool;
+// const { Pool } = require('pg');
+// require('dotenv').config();
+
+// const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+// const proConfig = process.env.DATABASE_URL;
+
+// const pool = new Pool({
+//   connectionString: process.env.NODE_ENV === 'production' ? proConfig : devConfig,
+// });
+
+// module.exports = pool;
+
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+pool.connect((err, client, done) => {
+  if (err) throw err;
+  console.log('Connected to the vercel database');
+});
+
+module.exports = pool;
+
